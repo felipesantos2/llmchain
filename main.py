373e1a3b816011b944255c4974a2c946e2fd66f8
felipesantos2 <santospinheiro627@gmail.com>
2026-04-13 16:46:55 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain.agents import create_agent
+from langchain.tools import tool, ToolRuntime
+from dataclasses import dataclass
+import getpass
+import os
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+
+@tool
+def get_weather_tool(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+agent = create_agent(
+    model="gemini-3.1-pro-preview",
+    tools=[get_weather_tool],
+    system_prompt="You are a helpful assistant",
+)
+
+def main():
+    agent.invoke(
+        {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+    )
+
+
+if __name__ == "__main__":
+    main()
